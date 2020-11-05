ae4465ba14e01ee70922b49b75bcf4ea18170f37
github 0322keisuke <isikeidesk@gmail.com>
2020-11-05 20:32:16 +0900

    一覧作成+アイコン決定

--- app/Http/Controllers/RoadmapController.php
+++ app/Http/Controllers/RoadmapController.php
@@ -6,5 +6,42 @@ use Illuminate\Http\Request;
 
 class RoadmapController extends Controller
 {
-    //
+    public function index()
+    {
+        //ダミーデータ
+        $roadmaps = [
+            (object)[
+                'id'=> 1,
+                'title' => 'ロードマップ１',
+                'body' => '本文1',
+                'created_at' => now(),
+                'user' => (object)[
+                    'id' => 1,
+                    'name' => 'ユーザー名1',
+                ],
+            ],
+            (object)[
+                'id'=>2,
+                'title' => 'ロードマップ2',
+                'body' => '本文2',
+                'created_at' => now(),
+                'user' => (object)[
+                    'id' => 2,
+                    'name' => 'ユーザー名2',
+                ],
+            ],
+            (object)[
+                'id'=>3,
+                'title' => 'ロードマップ3',
+                'body' => '本文3',
+                'created_at' => now(),
+                'user' => (object)[
+                    'id' => 3,
+                    'name' => 'ユーザー名3',
+                ],
+            ],
+        ];
+
+        return view('roadmaps.index',['roadmaps' => $roadmaps ]);
+    }
 }
