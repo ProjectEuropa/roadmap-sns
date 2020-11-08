1324e45fd45a862d115ff5e83e1eb0a6d616f3e9
github 0322keisuke <isikeidesk@gmail.com>
2020-11-08 15:51:40 +0900

    DBから教材名を取得・表示

--- app/Http/Controllers/TutorialController.php
+++ app/Http/Controllers/TutorialController.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Tutorial;
 use Illuminate\Http\Request;
 
 class TutorialController extends Controller
 {
     public function index(){
-        return view('tutorials.index');
+        $tutorials = Tutorial::all()->sortByDesc('created_at');
+
+        return view('tutorials.index',['tutorials' => $tutorials ]);
     }
 }
