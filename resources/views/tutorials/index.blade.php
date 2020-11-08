4d45f7d591e23a6d73267eec5604f84bc85cff16
github 0322keisuke <isikeidesk@gmail.com>
2020-11-09 08:02:40 +0900

    リストを追加

--- resources/views/tutorials/index.blade.php
+++ resources/views/tutorials/index.blade.php
@@ -5,17 +5,24 @@
 @section('content')
   @include('nav')
   <div class="container">
-    <div class="d-flex flex-row border p-3 mt-2">
+    <div class="d-flex flex-row border p-3 mt-1">
       @foreach($tutorials as $tutorial)
       <div class="border p-2 mr-1">{{ $tutorial->name }}</div>
       @endforeach
     </div>
     
-    <div class="row">
-      <div class="col col-md-4">
-      @foreach($tasks as $task)
-      <div class="border p-2 mr-1">{{ $task->name }}</div>
-      @endforeach
+    <div class="row mt-2">
+      <div class="col border col-md-4">
+        <h3 class="text-center"> Todo </h3>
+          @foreach($tasks as $task)
+          <div class="border p-2 m-1">{{ $task->name }}</div>
+          @endforeach
+      </div>
+      <div class="col border col-md-4">
+      <h3 class="text-center"> Doing </h3>
+      </div>
+      <div class="col border col-md-4">
+      <h3 class="text-center"> Done </h3>
       </div>
     </div>
   </div>
