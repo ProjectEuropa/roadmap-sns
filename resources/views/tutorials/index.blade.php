32e5fc3015f2f33a71a4e467be245d4a24338bed
github 0322keisuke <isikeidesk@gmail.com>
2020-11-08 10:43:28 +0900

    教材テーブルと教材モデルの作成

--- resources/views/tutorials/index.blade.php
+++ resources/views/tutorials/index.blade.php
@@ -5,7 +5,7 @@
 @section('content')
   @include('nav')
   <div class="container">
-    <div class="d-flex flex-row mt-2">
+    <div class="d-flex flex-row border p-3 mt-2">
       <div class="border p-2 mr-1">Flex item 1</div>
       <div class="border p-2 mr-1">Flex item 2</div>
       <div class="border p-2 mr-1">Flex item 3</div>
