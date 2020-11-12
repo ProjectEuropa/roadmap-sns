5d27e9ef448bc3fcab5c0f9079afe621556c927f
github 0322keisuke <isikeidesk@gmail.com>
2020-11-12 10:58:58 +0900

    いいね判定処理を追加

--- app/Roadmap.php
+++ app/Roadmap.php
@@ -4,6 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Roadmap extends Model
 {
@@ -11,4 +12,17 @@ class Roadmap extends Model
     {
         return $this->belongsTo('App\User');
     }
+
+    public function likes(): BelongsToMany
+    {
+        return $this->belongsToMany('App\User', ' likes')->withTimestamps();
+    }
+
+    public function isLikedBy(?User $user): bool
+    {
+        return $user
+            ? (bool)$this->likes->where('id', $user->id)->count()
+            : false;
+    }
+
 }
