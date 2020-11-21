920b7505bfc2a76b6059ee80e075ac470870700c
github 0322keisuke <isikeidesk@gmail.com>
2020-11-21 20:10:48 +0900

    actionsを追加

---
+++ resources/js/store/index.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+  },
+  mutations: {
+  },
+  actions: {
+    addlist(context,payload){
+      context.commit('addlist',payload)
+    },
+  },
+  getters: {
+  }
+})
