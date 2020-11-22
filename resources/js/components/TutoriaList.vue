18f5c9067a6d5b02203505b89ae83ebe9a1cd5c0
github 0322keisuke <isikeidesk@gmail.com>
2020-11-23 07:57:51 +0900

    Tutorial,TutorialListコンポーネントの作成

---
+++ resources/js/components/TutoriaList.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="list border p-2 mr-1">
+    <div class="listheader">
+      <p class="tutorial-title">{{ title }}</p>
+      <div class="deletelist" @click="removeTutorial">×</div>
+    </div>
+  </div>
+
+</template>
+
+<script>
+
+export default{
+  props:{
+    title:{
+      type:String,
+      required:true
+    },
+    listIndex:{
+      type:Number,
+      required:true
+    }
+  },
+  methods:{
+    removeTutorial:function(){
+      this.$store.dispatch('removeTutorial',{ listIndex:this.listIndex })
+    }
+  }
+}
+
+</script>
