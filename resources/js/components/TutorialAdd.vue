7107f444e19f3186e019d5da789ef489e2957aa6
github 0322keisuke <isikeidesk@gmail.com>
2020-11-22 12:58:43 +0900

    フォームがフォーカスされたら活性にする。

--- resources/js/components/TutorialAdd.vue
+++ resources/js/components/TutorialAdd.vue
@@ -4,8 +4,8 @@
     type="test"
     class="text-input"
     placeholder="新しい教材名を入力"
-    @focusin="startEditing"
-    @focusout="finishEditing"
+    @focusin= "startEditing"
+    @focusout= "finishEditing"
     >
     <button type="submit" class="teal accent-4 text-white border-0 rounded-pill">
       教材を追加
@@ -18,7 +18,7 @@ export default {
   data: function(){
     return {
       title: '',
-      idEditing: false,
+      isEditing: false,
     }
   },
   computed:{
