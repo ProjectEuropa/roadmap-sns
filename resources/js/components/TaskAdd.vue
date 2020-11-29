3f663bdbbcb2562951b2e4dc7dfcbd571d31b480
github 0322keisuke <isikeidesk@gmail.com>
2020-11-29 19:53:35 +0900

    TaskAdd.vueの追加

--- resources/js/components/TaskAdd.vue
+++ resources/js/components/TaskAdd.vue
@@ -1,5 +1,22 @@
 <template>
+  <form class="addtask" @submit.prevent="addTaskToList">
+    <input  v-model="body"
+            type="text"
+            class="text-input"
+            placeholder="タスクの追加"
+    />
+    <button type="submit" class="add-button">
+      Add
+    </button>
+  </form>
 </template>
 
 <script>
+export default {
+  data: function() {
+    return {
+      body: '',
+    }
+  },
+}
 </script>
