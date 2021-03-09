1573015ac86a3211cef69f2304f69ca3ed40c1b2
github 0322keisuke <isikeidesk@gmail.com>
2021-03-09 11:56:02 +0900

    未ログイン時の考慮

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,10 @@ Auth::routes();
 Route::get('/login/guest','Auth\LoginController@authenticate');
 Route::get('/','RoadmapController@index');
 
-Route::get('/tutorials', 'TutorialController@index')->name('tutorials.index');
-Route::post('/tutorials/store', 'TutorialController@store');
-Route::delete('/tutorials/{tutorial}/destroy', 'TutorialController@destroy');
+Route::get('/tutorials', 'TutorialController@index')->name('tutorials.index')->middleware('auth');
+Route::post('/tutorials/store', 'TutorialController@store')->middleware('auth');
+Route::delete('/tutorials/{tutorial}/destroy', 'TutorialController@destroy')->middleware('auth');
+
 
 
 Route::prefix('roadmaps')->name('roadmaps.')->group(function(){
