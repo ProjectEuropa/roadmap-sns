78f3b3bb08f64617f4cad1ed397cfeaa1d781662
github 0322keisuke <isikeidesk@gmail.com>
2021-03-16 18:26:50 +0900

    ロードマップ投稿画面の作成(Todoを除く)

--- resources/views/roadmaps/create.blade.php
+++ resources/views/roadmaps/create.blade.php
@@ -5,6 +5,20 @@
 @section('content')
   @include('nav')
   <div class="container">
-   ロードマップ投稿画面
+    <div class="row">
+      <div class="col-12">
+        <div class="card mt-3">
+          <div class="card-body pt-0">
+            <!-- @include('error_card_list') -->
+            <div class="card-text">
+              <form method="POST" action="{{ route('roadmaps.store') }}">
+                @include('roadmaps.form')
+                <button type="submit" class="btn teal accent-4 btn-block text-white">投稿する</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 @endsection
---
+++ resources/views/roadmaps/form.blade.php
@@ -0,0 +1,24 @@
+@csrf
+<div class="md-form">
+  <label for="title">ロードマップ名</label>
+  <input type="text" name="title" id="title" class="form-control" required value="{{ old('title') }}">
+</div>
+
+<div class="form-group">
+  <label></label>
+  <textarea name="body" class="form-control" required rows="8" placeholder="説明">{{ old('body') }}</textarea>
+</div>
+
+<div class="md-form">
+  <label for="estimated_time">学習時間目安(単位：時間)</label>
+  <input type="number" name="estimated_time" id="estimated_time" class="form-control" required value="{{ old('estimated_time') }}" min="1">
+</div>
+
+<div class="form-group">
+  <label>学習レベル</label>
+  <select name="level" class="form-control" requird value="{{ old('level') }}">
+  <option value="1">初級</option>
+  <option value="2">中級</option>
+  <option value="3">上級</option>
+  </select>
+</div>
